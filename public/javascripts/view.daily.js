20d34bb79072f3f8b82169d9f3abda4ad2752fe1
okunishinishi <okunishinishi@gmail.com>
2012-12-21 00:26:47 +0900

    add progress bar

--- public/javascripts/view.daily.js
+++ public/javascripts/view.daily.js
@@ -3,7 +3,19 @@
 
     });
     $.fn.extend({
-
+        progressBar:function(){
+            var bar = $(this).empty().addClass('progress-bar'),
+                rate = bar.data('rate'),
+                width = bar.width(),
+                div = '<div/>';
+            $(div)
+                .appendTo(bar)
+                .addClass('progress-filled')
+                .css({
+                    width:width * rate
+                });
+            return bar;
+        },
         issueSection:function(project){
             var section = $(this);
 
@@ -12,8 +24,14 @@
                 $.getJSON('issue/count', data, function(data){
                     console.log('get issue counts');
                 });
-
+                section.findByRole('progress-bar').progressBar();
             }
+            return section;
+        },
+        taskSection:function(){
+            var section = $(this);
+
+            section.findByRole('progress-bar').progressBar();
             return section;
         },
         keepInMindSection:function(){
@@ -40,11 +58,13 @@
 
 
         var issueSection = $('#issue-section', body).issueSection();
+        var taskSection = $('#task-section', body).taskSection();
         $('#user-project-select', body).change(function(){
             var select = $(this),
                 project = select.val();
             if(project){
                 issueSection.issueSection(project);
+                taskSection.taskSection();
             }
         });
 
