0c1223a9e621178f1bdb8880358750520f907863
okunishinishi <okunishinishi@gmail.com>
2012-12-23 21:20:52 +0900

    add test

--- routes/rt.team.js
+++ routes/rt.team.js
@@ -5,14 +5,20 @@
 var db = require('../db'),
     Team = db.models['Team'];
 
-/* /team以下全てを受け取る */
+/* /team下のrouteを全て受け取る */
 exports.all = function(req, res, next){
-    var team = req.session.team;
+    var team = req.session && req.session.team;
     var sameTeam = team && team.name == req.params.name;
     if(sameTeam){
         next();
     } else {
-        Team.findByName
-        res.redirect('/');
+        Team.findByName(req.params.team.name, function(team){
+            if(team){
+                req.session.team = team;
+                next();
+            } else {
+                res.redirect('/');
+            }
+        });
     }
 };
--- test/routes/mch.rt.team.js
+++ test/routes/mch.rt.team.js
@@ -12,6 +12,19 @@ var should = require('should'),
 describe('rt.team', function () {
     require('../check_env.js')();
 
+    before(function(done){
+        new Team({
+            name:'team01'
+        }).save(function(){
+                done();
+            });
+    });
+    after(function(done){
+        Team.prototype.connector.remove(function(){
+            done();
+        });
+    });
+
     it('all (invalid team name)', function(done){
         route.all(new Req({
             session:{
@@ -26,5 +39,24 @@ describe('rt.team', function () {
                 done();
             }
         }));
+    });
+    it('all (change team name', function(done){
+        var req = new Req({
+            session:{
+                team:{
+                    name:'old_team'
+                }
+            },
+            params:{
+                team:{
+                    name:'team01'
+                }
+            }
+        });
+        var res = new Res({});
+        route.all(req, res, function(){
+            req.session.team.should.have.property('name', 'team01');
+            done();
+        });
     });
 });
