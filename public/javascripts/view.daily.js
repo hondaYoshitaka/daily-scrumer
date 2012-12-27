901da50efd6e7872fd20c4b9ab32104e5c30f7cf
okunishinishi <okunishinishi@gmail.com>
2012-12-27 20:44:27 +0900

    remove chart

--- public/javascripts/view.daily.js
+++ public/javascripts/view.daily.js
@@ -161,59 +161,6 @@
 
             return section;
         },
-        reportChart:function (data) {
-            var chart = $(this),
-                id = chart.attr('id');
-
-            new Highcharts.Chart({
-                title:{
-                    text:'report'
-                },
-                chart:{
-                    renderTo:id,
-                    type:'line'
-                },
-                xAxis:{
-                    type:'datetime'
-                },
-                yAxis:{
-                    title:{
-                        text:'y title'//TODO
-                    }
-                },
-//                series:data.series
-                series:[
-                    {
-                        name:'Tokyo',
-                        data:[7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-                    },
-                    {
-                        name:'New York',
-                        data:[-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-                    },
-                    {
-                        name:'Berlin',
-                        data:[-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-                    },
-                    {
-                        name:'London',
-                        data:[3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-                    }
-                ]
-            });
-
-            return chart;
-        },
-        reportChartSection:function () {
-            var section = $(this).addClass('report-chart-section');
-            var data = {
-                series:[
-
-                ]
-            };
-            $('#report-chart', section).reportChart(data);
-            return section;
-        },
         groupingRouletteItem:function (data) {
             return $(this).each(function () {
                 var item = $(this)
@@ -376,7 +323,6 @@
 
         $('#keep-in-mind-section', body).keepInMindSection();
         $('#calendar-section', body).calendarSection();
-        $('#report-chart-section', body).reportChartSection();
         $('#grouping-section', body).groupingSection();
     });
 })(jQuery);
