d0998bbbbc6d26d7efeae7d4538a3c72c04ac014
okunishinishi <okunishinishi@gmail.com>
2013-01-09 17:00:44 +0900

    add story list

--- public/javascripts/view.story.js
+++ public/javascripts/view.story.js
@@ -1,5 +1,41 @@
 (function ($) {
     $.fn.extend({
+        storyListItem:function () {
+            return $(this).each(function () {
+
+            });
+        },
+        storyList:function (data) {
+            var ul = $(this);
+
+            var tmpl = Handlebars.templates['tmpl.story-list-item'];
+            data.forEach(function (data) {
+                $(tmpl(data)).appendTo(ul)
+                    .storyListItem();
+            });
+            return ul;
+        },
+        storyArticle:function () {
+            var article = $(this);
+            var storyList = $('#story-list', article);
+
+            article.showSpin();
+            var data = {
+                sprint:CS.sprint,
+                team_id:CS.team._id
+            };
+            $.get('/sprint/get_stories', data, function (data) {
+                article.removeSpin();
+                if (data.success) {
+                    CS.stories = data.stories;
+                    storyList.storyList(CS.stories);
+                } else {
+                    console.error('failed to load stories');
+                }
+            });
+            return article;
+        }
+
 
     });
     $(function () {
@@ -8,18 +44,9 @@
 
         $('#head-nav', body).nav('story');
 
-        var data = {
-            sprint:CS.sprint,
-            team_id:CS.team._id
-        };
-        $.get('/sprint/get_stories', data, function (data) {
-            if (data.success) {
-                console.log('sprint', data.stories);
-                CS.stories = data.stories;
-            } else {
-                console.error('failed to load stories');
-            }
-        });
+
+        $('#story-article', body).storyArticle();
+
 
     });
 })(jQuery);
