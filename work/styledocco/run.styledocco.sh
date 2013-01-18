9763a610a7c8178148492cb9becb5a7babf2f6c3
okunishinishi <okunishinishi@gmail.com>
2013-01-18 10:49:42 +0900

    modify shell

--- work/styledocco/run.styledocco.sh
+++ work/styledocco/run.styledocco.sh
@@ -1,3 +1,3 @@
 PATH=/usr/local/mysql/bin/:/Library/Frameworks/Python.framework/Versions/2.7/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/opt/local/bin:/opt/local/sbin:/sw/bin:/sw/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/usr/X11R6/bin
 
-styledocco --out ../../public/html/styledocco -n "Style Guilde" ./style.css
+styledocco --out ../../public/html/styledocco -n "Style Guilde" ./*.css
